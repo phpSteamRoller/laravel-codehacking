6bb6b2fe5875714e9571386c182c61bb93cd9528
SteamRoller <valjak.dragan@gmail.com>
2019-03-21 20:35:17 +0100

    Users page working without photos

--- app/Http/Controllers/AdminUsersController.php
+++ app/Http/Controllers/AdminUsersController.php
@@ -2,9 +2,12 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
+use App\Role;
 
+use App\User;
 use App\Http\Requests;
+use Illuminate\Http\Request;
+use App\Http\Requests\UserRequest;
 
 class AdminUsersController extends Controller
 {
@@ -15,7 +18,8 @@ class AdminUsersController extends Controller
      */
     public function index()
     {
-        return view('admin.users.index');
+        $users = User::all();
+        return view('admin.users.index', compact('users'));
     }
 
     /**
@@ -25,7 +29,10 @@ class AdminUsersController extends Controller
      */
     public function create()
     {
-        return view('admin.users.create');
+
+        $roles = Role::lists('name','id')->all();
+       //dd($roles);
+        return view('admin.users.create', compact('roles'));
     }
 
     /**
@@ -34,9 +41,11 @@ class AdminUsersController extends Controller
      * @param  \Illuminate\Http\Request  $request
      * @return \Illuminate\Http\Response
      */
-    public function store(Request $request)
+    public function store(UserRequest $request)
     {
-        //
+        User::create($request->all());
+
+        return redirect('/admin/users');
     }
 
     /**
--- resources/views/admin/users/index.blade.php
+++ resources/views/admin/users/index.blade.php
@@ -1,1 +1,39 @@
-USERS
+@extends('layouts.admin')
+
+
+@section('content')
+
+
+
+<h1>USERS</h1>
+<table class="table">
+    <thead>
+        <tr>
+            <th>Id</th>
+            <th>Name</th>
+            <th>Email</th>
+            <th>Role</th>
+            <th>Active</th>
+            <th>Cretaed</th>
+        </tr>
+    </thead>
+    <tbody>
+        @if ($users)
+        @foreach ($users as $user)
+        <tr>
+                <td scope="row">{{ $user->id }}</td>
+                <td>{{ $user->name }}</td>
+                <td>{{ $user->email }}</td>
+                <td>{{ $user->role->name }}</td>
+                <td>{{ $user->is_active == 1 ? 'Active': 'Not active' }}</td>
+                <td>{{ $user->created_at }}</td>
+            </tr>
+        @endforeach
+      
+        @endif
+       
+     
+    </tbody>
+</table>
+    
+@endsection
