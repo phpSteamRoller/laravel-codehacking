510dd32b01c2376b3138028cd21884d4405aa967
SteamRoller <valjak.dragan@gmail.com>
2019-03-24 10:37:24 +0100

    Add photo to users

--- resources/views/admin/users/create.blade.php
+++ resources/views/admin/users/create.blade.php
@@ -25,8 +25,8 @@
 </div>
 
 <div class="form-group">
-        {!! Form::label('file', 'User Photo') !!}
-        {!! Form::file('file', null, ['class'=>'form-control']) !!}
+        {!! Form::label('photo_id', 'User Photo') !!}
+        {!! Form::file('photo_id', null, ['class'=>'form-control']) !!}
 </div>
 
 <div class="form-group">
